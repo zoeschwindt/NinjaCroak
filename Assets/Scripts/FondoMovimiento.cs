3a89a32963522baa3f89cf5e1166fa9fb3fb806d
zoeschwindt <schwindtzoe380@gmail.com>
2025-08-01 20:31:15 -0300

    Fondo con efecto Parallax

---
+++ Assets/Scripts/FondoMovimiento.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FondoMovimiento : MonoBehaviour
+{
+    [SerializeField] private Vector2 velocidadMovimiento;
+
+    private Vector2 offset;
+
+    private Material material;
+
+    private Rigidbody2D jugadorRB;
+
+    private void Awake()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+        jugadorRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update()
+    {
+        offset = (jugadorRB.linearVelocity.x * 0.1f) * velocidadMovimiento * Time.deltaTime;
+        material.mainTextureOffset += offset;
+    }
+
+
+
+}
