b11a4eccefa273ddda50f5e4b0917eb4004a7de1
zoeschwindt <schwindtzoe380@gmail.com>
2025-08-01 21:12:33 -0300

    Creacion de script de clase Jefe

---
+++ Assets/Scripts/BossEnemy.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BossEnemy : Enemy
+{
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float shootCooldown = 2f;
+    private float shootTimer;
+
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if (shootTimer >= shootCooldown)
+        {
+            Shoot();
+            shootTimer = 0f;
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+    }
+}
