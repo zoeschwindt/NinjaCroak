b5bae8e38705d49504302f9941970367a49d33fd
zoeschwindt <schwindtzoe380@gmail.com>
2025-08-01 21:17:42 -0300

    Creacion de script para disparo

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float speed = 10f;
+    public int damage = 20;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<IDamageable>(out var target))
+        {
+            target.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
