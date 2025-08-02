ae6e87a6f224ef76a749a9d561a827f61d673863
zoeschwindt <schwindtzoe380@gmail.com>
2025-08-02 03:05:33 -0300

    Script AdvancedEnemy (patrullaje + persecución + disparo)

---
+++ Assets/AdvancedEnemy.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class AdvancedEnemy : Enemy
+{
+    [Header("Patrullaje")]
+    public Transform puntoIzquierdo;
+    public Transform puntoDerecho;
+    private bool moviendoDerecha = true;
+
+    [Header("Persecución")]
+    public float rangoPersecucion = 5f;
+    private Transform player;
+
+    [Header("Ataque a distancia")]
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float rangoDisparo = 3f;
+    public float tiempoEntreDisparos = 2f;
+    private float temporizadorDisparo;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        float distanciaJugador = Vector2.Distance(transform.position, player.position);
+
+        if (distanciaJugador <= rangoPersecucion)
+        {
+            PerseguirJugador();
+
+            if (distanciaJugador <= rangoDisparo)
+            {
+                Disparar();
+            }
+        }
+        else
+        {
+            Patrullar();
+        }
+    }
+
+    private void Patrullar()
+    {
+        if (moviendoDerecha)
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            if (transform.position.x >= puntoDerecho.position.x)
+                moviendoDerecha = false;
+        }
+        else
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            if (transform.position.x <= puntoIzquierdo.position.x)
+                moviendoDerecha = true;
+        }
+    }
+
+    private void PerseguirJugador()
+    {
+        if (player.position.x > transform.position.x)
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        else
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+
+    private void Disparar()
+    {
+        temporizadorDisparo += Time.deltaTime;
+
+        if (temporizadorDisparo >= tiempoEntreDisparos)
+        {
+            Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+            temporizadorDisparo = 0f;
+        }
+    }
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealthUI health = collision.gameObject.GetComponent<PlayerHealthUI>();
+            if (health != null)
+            {
+                health.TakeDamage(1);
+            }
+        }
+    }
+}
